18310961dd6a0d335d77af06c0e7849cb29c290e
Geovanna-School <141353907+Geovanna-School@users.noreply.github.com>
2023-08-17 16:17:40 -0300

    Add files via upload

---
+++ phpoo/CafeBistro/conexao.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "cafebistro_phpoo_turmaA";
+
+//criação da conexão
+$conn = new mysqli($servername, $username, $password, $databasename);
+
+//verificando a conexão 
+if (!$conn){
+    die("conexão falhou".mysqli_connect_error());
+}else{
+    echo "conectou";
+};
+
+
+?>
