22db6b328cd354d5d5468ded6ba35f2fb366a36d
Geovanna-School <141353907+Geovanna-School@users.noreply.github.com>
2023-08-31 16:14:09 -0300

    Add files via upload

--- phpoo/CafeBistro/processar-login.php
+++ phpoo/CafeBistro/processar-login.php
@@ -1,12 +1,12 @@
 <?php
 require "conexao.php";
-require "autenticacao.php";
+require "Autenticacao.php";
 
 if ($_SERVER["REQUEST_METHOD"] =="POST"){
     $email = $_POST["email"];
     $senha = $_POST["senha"];
-
-    $login = new Autenticacao($conn);
+    
+    $login = new autenticacao($conn);
     $usuario = $login->verificarUsuario($email, $senha);
     if ($usuario){
         session_start();
@@ -14,9 +14,8 @@ if ($_SERVER["REQUEST_METHOD"] =="POST"){
         header("Location: index.php");
         exit;
     }else{
+
         header("Location: login.php?erro=1");
     }
 }
-
-
 ?>
